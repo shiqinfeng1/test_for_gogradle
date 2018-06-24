16b81a380dd035a62678b238e08c2c527b9fb731
shiqinfeng <shiqinfeng@shiqinfengdeMacBook-Pro.local>
2018-06-24 11:18:06 +0800

    add new cmd

---
+++ app/cmd/hello.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+  	rootCmd.AddCommand(helloCmd)
+}
+
+var helloCmd = &cobra.Command{
+	Short: "hello",
+	Args: func(cmd *cobra.Command, args []string) error {
+	  if len(args) < 1 {
+		return errors.New("requires at least one arg")
+	  }
+	  /*
+	  if myapp.IsValidColor(args[0]) {
+		return nil
+	  }
+	  */
+	  return fmt.Errorf("invalid color specified: %s", args[0])
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+	  fmt.Println("Hello, World!")
+	},
+  }
---
+++ app/cmd/root.go
@@ -0,0 +1,75 @@
+package cmd 
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+  
+var rootCmd = &cobra.Command{
+	Use:   "hugo",
+	Short: "Hugo is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
+					love by spf13 and friends in Go.
+					Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	
+	//Persistent类型的参数表示在子命令种也有效， 相当于全局参数。对应的，设置本地参数的方法是：Flags()
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&projectBase, "projectbase", "b", "", "base project directory eg. github.com/spf13/")
+	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "Author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "Name of license for the project (can provide `licensetext` in config)")
+	rootCmd.PersistentFlags().Bool("viper", true, "Use Viper for configuration")
+	
+	//指定启动时必须输入的参数
+	rootCmd.Flags().StringVarP(&Region, "region", "r", "", "AWS region (required)")
+	rootCmd.MarkFlagRequired("region")	
+	
+	//将输入参数和配置文件中的字段绑定，如果运行时不带有该输入参数，则不会从配置文件中读入对应的字段
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.BindPFlag("projectbase", rootCmd.PersistentFlags().Lookup("projectbase"))
+	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
+	viper.SetDefault("license", "apache")
+
+	
+}
+
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		}
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cobra")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
+		os.Exit(1)
+	}
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
